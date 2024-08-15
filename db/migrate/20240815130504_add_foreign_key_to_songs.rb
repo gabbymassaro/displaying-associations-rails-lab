769393db2b27ef53c79b7f05fb33ddf98b0a0c0c
Gabriella Massaro <gmassaro807@gmail.com>
2024-08-15 11:27:54 -0400

    Pass all tests

---
+++ db/migrate/20240815130504_add_foreign_key_to_songs.rb
@@ -0,0 +1,5 @@
+class AddForeignKeyToSongs < ActiveRecord::Migration[5.0]
+  def change
+    add_foreign_key :songs, :artists, column: :artist_id
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# db/seeds.rb
+Artist.destroy_all
+Song.destroy_all
+
+puts "Seeding artists...🌱"
+artist1 = Artist.create(name: "The Beatles")
+artist2 = Artist.create(name: "Led Zeppelin")
+artist3 = Artist.create(name: "Pink Floyd")
+artist4 = Artist.create(name: "The Rolling Stones")
+artist5 = Artist.create(name: "Queen")
+
+puts "Seeding songs...🌱"
+Song.create(title: "Hey Jude", artist: artist1)
+Song.create(title: "Let It Be", artist: artist1)
+Song.create(title: "Stairway to Heaven", artist: artist2)
+Song.create(title: "Whole Lotta Love", artist: artist2)
+Song.create(title: "Comfortably Numb", artist: artist3)
+Song.create(title: "Wish You Were Here", artist: artist3)
+Song.create(title: "Paint It Black", artist: artist4)
+Song.create(title: "Sympathy for the Devil", artist: artist4)
+Song.create(title: "Bohemian Rhapsody", artist: artist5)
+Song.create(title: "We Will Rock You", artist: artist5)
